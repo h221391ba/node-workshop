5b50563fa14ea109e9b9571bad7933d9b07217f0
Reverie <83707139+h221391ba@users.noreply.github.com>
2021-08-06 22:12:31 +0800

    用 async/await 改寫作業1

--- promise/promise1.js
+++ promise/promise1.js
@@ -1,6 +1,6 @@
-//作業二
+//作業三
 // 模擬一個非同步工作
-let doWork = function (job, timer, isOK) {
+function doWork (job, timer, isOK) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             let dt = new Date();
@@ -13,25 +13,28 @@ let doWork = function (job, timer, isOK) {
     });
 };
 
-let job1 = doWork("刷牙", 3000, true);
-// console.log(job1);
+let work = async ()=>{
+    console.log(await doWork("刷牙", 3000, true),'\n',await doWork("吃飯", 3000, true),'\n',await doWork("洗澡", 3000, true));
+}
+work();
 
-job1
-    .then((result) => {
-        console.log('job1 finished', result);
-        return doWork('划手機', 5000, true);
-    })
-    .then((result) => {
-        console.log('job2 finished', result);
-        return doWork('玩遊戲', 8000, true);
-    })
-    .then((result) => {
-        console.log('job3 finished', result);
-    })
-    .catch((error) => {
-        console.log(error);
-    })
-    .finally(() => {
-        console.log('well done');
-    });
+
+// job1
+//     .then((result) => {
+//         console.log('job1 finished', result);
+//         return doWork('划手機', 5000, true);
+//     })
+//     .then((result) => {
+//         console.log('job2 finished', result);
+//         return doWork('玩遊戲', 8000, true);
+//     })
+//     .then((result) => {
+//         console.log('job3 finished', result);
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//     })
+//     .finally(() => {
+//         console.log('well done');
+//     });
 
