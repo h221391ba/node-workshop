b7efb27d82b96fe2b2c4f3b6632f88f4628639c7
Reverie <83707139+h221391ba@users.noreply.github.com>
2021-08-06 15:42:03 +0800

    add promise1.js

---
+++ promise/promise1.js
@@ -0,0 +1,16 @@
+// 模擬一個非同步工作
+let doWork = function (job, timer, isOK) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let dt = new Date();
+            if (isOK) {
+                console.log(`完成工作: ${job} at ${dt.toISOString()}`);
+            } else {
+                console.log(`失敗了 ${job}`);
+            }
+        }, timer);
+    });
+};
+
+let job1 = doWork("刷牙", 3000, true);
+console.log(job1);
