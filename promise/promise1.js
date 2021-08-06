2e4c86033d2376f6401467641b9d68c6f76c5d57
Reverie <83707139+h221391ba@users.noreply.github.com>
2021-08-06 21:48:29 +0800

    用 promise.then 解決 callback hell

--- promise/promise1.js
+++ promise/promise1.js
@@ -1,4 +1,4 @@
-//作業一
+//作業二
 // 模擬一個非同步工作
 let doWork = function (job, timer, isOK) {
     return new Promise((resolve, reject) => {
@@ -6,13 +6,31 @@ let doWork = function (job, timer, isOK) {
             let dt = new Date();
             if (isOK) {
                 console.log(`完成工作: ${job} at ${dt.toISOString()}`);
+                resolve();
             } else {
                 console.log(`失敗了 ${job}`);
+                reject();
             }
         }, timer);
     });
 };
 
 let job1 = doWork("刷牙", 3000, true);
-console.log(job1);
+// console.log(job1);
+
+job1
+    .then((result) => {
+        console.log('job1 finished');
+        return doWork('划手機', 5000, true);
+    })
+    .then((result) => {
+        console.log('job2 finished');
+        return doWork('玩遊戲', 8000, true);
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+    .finally(() => {
+        console.log('well done');
+    });
 
