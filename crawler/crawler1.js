0a4764f201848f7e38a9523ce75fbf52913c3bab
Reverie <83707139+h221391ba@users.noreply.github.com>
2021-08-07 20:07:20 +0800

    作業1 readfile callback 版

--- crawler/crawler1.js
+++ crawler/crawler1.js
@@ -1,18 +1,29 @@
 const axios = require('axios');
 const moment = require('moment');
+const fs = require("fs");
+const file = 'stock.txt';
 
-console.log(moment().format('YYYYMMDD'));
+// console.log(moment().format('YYYYMMDD'));
 
-axios.get('https://www.twse.com.tw/exchangeReport/STOCK_DAY', {
-    params:{
-        response: 'json',
-        date: moment().format('YYYYMMDD'),
-        stockNo: 2330
-    },
+fs.readFile(file, 'utf8', (err, stockCode) => {
+    if (err) {
+        console.log(err);
+    } else {
+        axios.get('https://www.twse.com.tw/exchangeReport/STOCK_DAY', {
+            params: {
+                response: 'json',
+                date: moment().format('YYYYMMDD'),
+                stockNo: stockCode
+            },
+        })
+            .then(res => {
+                console.log(res.data.title);
+            })
+            .catch(err => {
+                console.error(err);
+            })
+    }
 })
-    .then(res => {
-        console.log(res.data);
-    })
-    .catch(err => {
-        console.error(err);
-    })
+
+
+
