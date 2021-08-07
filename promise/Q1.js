d4b7d43c6a0e84ec9b5ec70dd8ea9359d5ffea94
Reverie <83707139+h221391ba@users.noreply.github.com>
2021-08-07 09:47:09 +0800

    fininsh Q1

--- promise/Q1.js
+++ promise/Q1.js
@@ -12,3 +12,11 @@ function syncF() {
   console.log(4);
   syncF();
   console.log(5);
+
+  /**
+   * 4 1 3 5 2
+   * 
+   * line 12 先做 --> 
+   * line 13 呼叫函式，執行setTimeout，由於異步特性所以裡面的東西給底層therad做，並不會等待裡面的function做完，因此接續 line 9，syncF()結束 -> 
+   * line 14 console.log
+   *  */ 
