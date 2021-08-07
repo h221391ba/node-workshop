38a75d7c30f5cfe27aafa91ba9a9e899f0105d37
Reverie <83707139+h221391ba@users.noreply.github.com>
2021-08-07 16:10:09 +0800

    除錯

--- crawler/crawler1.js
+++ crawler/crawler1.js
@@ -3,8 +3,8 @@ const moment = require('moment');
 
 console.log(moment().format('YYYYMMDD'));
 
-axios.get('https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20210807&stockNo=2330', {
-    parame:{
+axios.get('https://www.twse.com.tw/exchangeReport/STOCK_DAY', {
+    params:{
         response: 'json',
         date: moment().format('YYYYMMDD'),
         stockNo: 2330
