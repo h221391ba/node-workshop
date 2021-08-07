e353bcf91b78263c4ac19190c4c2a9a91b6dfeb7
Reverie <83707139+h221391ba@users.noreply.github.com>
2021-08-07 20:35:13 +0800

    作業3 (挑戰題) async/await 版

--- crawler/crawler2.js
+++ crawler/crawler2.js
@@ -5,32 +5,38 @@ const file = 'stock.txt';
 
 // console.log(moment().format('YYYYMMDD'));
 
-new Promise((res, rej) => {
-    fs.readFile(file, 'utf8', (err, stockCode) => {
-        if (err) {
-            rej(err);
-        } else {
-            res(stockCode);
-        }
-    })
-})
-    .then(result => {
-        return axios.get('https://www.twse.com.tw/exchangeReport/STOCK_DAY', {
-            params: {
-                response: 'json',
-                date: moment().format('YYYYMMDD'),
-                stockNo: result
+let getStockCode = () => {
+    return new Promise((res, rej) => {
+        fs.readFile(file, 'utf8', (err, stockCode) => {
+            if (err) {
+                rej(err);
+            } else {
+                res(stockCode);
             }
         })
-
     })
-    .then(result => {
+}
+
+let axiosGet = (stockCode) => {
+    return axios.get('https://www.twse.com.tw/exchangeReport/STOCK_DAY', {
+        params: {
+            response: 'json',
+            date: moment().format('YYYYMMDD'),
+            stockNo: stockCode
+        }
+    })
+}
+
+let homework = async () => {
+    try {
+        let stockCode = await getStockCode();
+        let result = await axiosGet(stockCode);
         console.log(result.data.title);
-    })
-    .catch(err => {
-        console.error(err);
-    })
+    } catch (err) { 
+        console.log(err);
+    }
 
+}
 
-
+homework();
 
