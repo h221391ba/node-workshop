f0036de2ba0da3a76a6209583ff5027a86cf716b
Reverie <83707139+h221391ba@users.noreply.github.com>
2021-08-15 11:26:25 +0800

    開發兩個 middleware

--- simple-express/server.js
+++ simple-express/server.js
@@ -3,6 +3,18 @@ const express = require("express");
 // 利用 express 建立了一個 express application
 let app = express();
 
+app.use((req, res, next) => {
+    let dt = new Date;
+    console.log(`middleware01${dt}`);
+    next();
+});
+
+app.use((req, res, next) => {
+    let dt = new Date;
+    console.log(`middleware02${dt}`);
+    next();
+});
+
 // HTTP Method: get, post, put, patch, delete
 app.get("/", function (request, response, next) {
     response.send("Hello");
